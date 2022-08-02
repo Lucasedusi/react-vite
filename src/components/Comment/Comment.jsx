ed091f73fa71b0ac7a43ccac49f1a76ecf3885a2
lucasedusi <lucasmto.16@hotmail.com>
2022-08-02 16:42:16 -0300

    feat: create comment component

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+	return (
+		<div className={styles.comment}>
+			<img className={styles.avatar} src="https://github.com/lucasedusi.png" />
+
+			<div className={styles.commentBox}>
+				<div className={styles.commentContent}>
+					<header>
+						<div className={styles.authorAndTime}>
+							<strong>Lucas Eduardo</strong>
+
+							<time dateTime="2022-08-02 14:23:45">Publicado há 1 hora</time>
+						</div>
+
+						<button>
+							<Trash size={20} />
+						</button>
+					</header>
+
+					<p>Muito bom, Parabéns</p>
+				</div>
+
+				<footer>
+					<button>
+						<ThumbsUp /> Aplaudir <span>20</span>
+					</button>
+				</footer>
+			</div>
+		</div>
+	);
+}
